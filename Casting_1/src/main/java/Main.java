960d5e2c95435983582789132be42cd14af6c811
N0F4C3x <andrew-1999@hotmail.it>
2025-10-07 20:18:52 +0200

    Casting strighe e caratteri

---
+++ Casting_1/src/main/java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+  // Scrivere uno snippet di codice che dati 2 numeri interi(int) li sommi e li stampi con la virgola (double).
+
+        int a = 5;
+        int b = 2;
+        double somma = (double)a + (double)b;
+
+        System.out.println(somma);
+
+
+
+ // Con due caratteri a e b, fare il casting in intero (int) e la somma
+
+        char c = 'c';
+        char d = 'd';
+        int sommacar = (int)c + (int)d;
+
+        System.out.println(sommacar);
+
+
+
+
+
+
+
+    }
+}
