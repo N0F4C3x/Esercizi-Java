2e576cfe29db739dbba06b487989cceb0b793cb9
N0F4C3x <andrew-1999@hotmail.it>
2025-10-07 19:52:02 +0200

    Esercizio area triangolo

---
+++ prova/src/Main.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+      // data base (3,5) e altezza (4,5) calcola area triangolo
+
+
+        double base = 3.5;
+        double altezza = 4.5;
+
+        System.out.println("Il valore dell'area è: " + base * altezza);
+
+
+
+
+
+
+
+
+
+    }
+}
+
